88342a6150ff7fbec87be60cbe1c5c6a728c3ba6
hyochan <dooboolab@gmail.com>
2020-02-08 19:29:23 +0900

    Correctly add ios icon

--- android/app/src/main/java/com/doobooiap/MainActivity.java
+++ android/app/src/main/java/com/doobooiap/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "doobooiap";
+        return "DoobooIAP";
     }
 
     @Override
