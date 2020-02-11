739814d7db6dc2231e87a812dbe3f426ad723dcc
Hyo Chan Jang <dooboolab@gmail.com>
2020-02-11 21:06:53 +0900

    Release configs (#1)
    * Release configs
    * Correctly add ios icon
    * Install react-native-iap
    * Install splash screen

--- android/app/src/main/java/com/doobooiap/MainActivity.java
+++ android/app/src/main/java/com/doobooiap/MainActivity.java
@@ -1,19 +1,25 @@
 package com.doobooiap;
 
+import android.os.Bundle;
+import org.devio.rn.splashscreen.SplashScreen;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this, R.style.SplashScreenTheme);
+        super.onCreate(savedInstanceState);
+    }
     /**
      * Returns the name of the main component registered from JavaScript. This is
      * used to schedule rendering of the component.
      */
     @Override
     protected String getMainComponentName() {
-        return "doobooiap";
+        return "DoobooIAP";
     }
 
     @Override
@@ -22,7 +28,11 @@ public class MainActivity extends ReactActivity {
             @Override
             protected ReactRootView createRootView() {
                 return new RNGestureHandlerEnabledRootView(MainActivity.this);
-            }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this, R.style.SplashScreenTheme);
+        super.onCreate(savedInstanceState);
+    }            }
         };
     }
 }
