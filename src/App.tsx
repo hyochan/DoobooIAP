477e8085615760da3b5484433f7cd2fed9801f60
hyochan <dooboolab@gmail.com>
2020-02-11 21:06:21 +0900

    Install splash screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
 import RootNavigator from './components/navigation/RootStackNavigator';
 import RootProvider from './providers';
+import SplashScreen from 'react-native-splash-screen';
 
 function App(): React.ReactElement {
   return <RootNavigator />;
 }
 
 function ProviderWrapper(): React.ReactElement {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <RootProvider>
       <App />
