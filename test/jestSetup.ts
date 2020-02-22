5bc20dd46cbe8dd8e8212825a939dcf92589386c
hyochan <dooboolab@gmail.com>
2020-02-23 00:13:25 +0900

    Update test and snapshot

--- test/jestSetup.ts
+++ test/jestSetup.ts
@@ -4,9 +4,6 @@ import { GlobalWithFetchMock } from 'jest-fetch-mock';
  * Something went wrong initializing the native ReactLocalization module
  * https://gist.github.com/MoOx/08b465c3eac9e36e683929532472d1e0
  */
-import mockAsyncStorage from '@react-native-community/async-storage/jest/async-storage-mock';
-
-jest.mock('@react-native-community/async-storage', () => mockAsyncStorage);
 jest.mock('react-native-reanimated', () =>
   require('react-native-reanimated/mock'),
 );
