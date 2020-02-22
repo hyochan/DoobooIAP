6fee65ac49812f8f9efbbd74af2fc0e442da6241
Hyo Chan Jang <dooboolab@gmail.com>
2020-02-23 02:20:04 +0900

    Firebase integratio, release config and ci (#5)
    * Add firebase config
    
    * Firebase initialization
    
    * Add firestore config
    
    * Update config
    
    * Update readme and postinstall jetify
    
    * build.gradle release
    
    * Fix build.gradle
    
    * Post react-native-config setup for android
    
    * Fix MainActivity syntax error

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,39 @@
+import 'firebase/firestore';
+
 import React, { useEffect } from 'react';
 
+import Config from 'react-native-config';
 import RootNavigator from './components/navigation/RootStackNavigator';
 import RootProvider from './providers';
 import SplashScreen from 'react-native-splash-screen';
 import { ThemeType } from '@dooboo-ui/native-theme';
+import firebase from 'firebase';
 import { useDarkMode } from 'react-native-dark-mode';
+
+const {
+  apiKey,
+  authDomain,
+  databaseURL,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+  measurementId,
+} = Config;
+
+const firebaseConfig = {
+  apiKey,
+  authDomain,
+  databaseURL,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+  measurementId,
+};
+!firebase.apps.length
+  ? firebase.initializeApp(firebaseConfig).firestore()
+  : firebase.app().firestore();
 
 function App(): React.ReactElement {
   return <RootNavigator />;
